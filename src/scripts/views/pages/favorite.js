98b61ba5febb4fe14930c0f5af39a41ed8f7a3cd
Syahrul-Prayoga <syahrulprayoga@gmail.com>
2021-07-02 03:30:03 +0800

    show favorite restaurant data from indexDB

--- src/scripts/views/pages/favorite.js
+++ src/scripts/views/pages/favorite.js
@@ -1,15 +1,25 @@
+import FavoriteRestaurantIdb from "../../data/restaurant-idb";
+import { createRestaurantItemTemplate } from "../templates/template-creator";
+
 const Favorite = {
   async render() {
     return `
     <div class="restaurant">
     <h1 tabindex="0" class="restaurant__label">Tempat Favorit</h1>
     <section id="posts" class="list__restaurant">
+
+    </section>
+    </div>
     `;
   },
- 
+
   async afterRender() {
-    // Fungsi ini akan dipanggil setelah render()
+    const restaurants = await FavoriteRestaurantIdb.getAllRestaurant();
+    const postRestaurant = document.querySelector('#posts');
+    restaurants.forEach(restaurant => {
+      postRestaurant.innerHTML += createRestaurantItemTemplate(restaurant);
+    });
   },
 };
- 
+
 export default Favorite;
