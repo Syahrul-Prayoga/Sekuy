92147f5dc954ec542a3450a3956280752a8ddf21
Syahrul-Prayoga <syahrulprayoga@gmail.com>
2021-07-02 21:59:53 +0800

    Reinstall Eslint

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,12 @@
+/* eslint-disable import/no-useless-path-segments */
+/* eslint-disable import/extensions */
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
 import '../scripts/components/navbar.js';
 import '../scripts/components/hero.js';
 import '../scripts/components/footer.js';
-import data from '../DATA.json';
 import App from './views/app';
-
-// nav button in mobile 
-// const menu = document.querySelector('#menu');
-// const drawer = document.querySelector('#drawer');
-// const main = document.querySelector('main');
-// const hero = document.querySelector('.hero');
 
 const app = new App({
   button: document.querySelector('#menu'),
@@ -30,33 +25,3 @@ window.addEventListener('hashchange', () => {
 window.addEventListener('load', () => {
   app.renderPage();
 });
-
-
-// Render Data
-
-// const getData = (data) => {
-//   const posts = document.querySelector('#posts');
-//   posts.innerHTML="";
-
-//   data.restaurants.forEach((resto) => {
-//     posts.innerHTML +=
-//     /*html*/
-//     `
-//     <div class="card">
-//       <a href="#">
-//         <div class="card__img">
-//           <img class="card__thumbnail" src="${resto.pictureId}" alt="${resto.name}">
-//           <div class="card__rating">${resto.rating}</div>
-//           <div class="card__title">${resto.name} - ${resto.city}</div>
-//         </div>
-//         <div class="card__content">
-//           <p class="card__content__title">Description</p>
-//           <p class="card__content__desc">${resto.description}</p>
-//         </div>
-//       </a>
-//     </div>
-//     `;
-//   })
-// }
-// getData(data);
-
