4c3c56f3b2a8320e846c50bb41fe955e92875b63
Syahrul-Prayoga <syahrulprayoga@gmail.com>
2021-07-03 01:06:22 +0800

    setup cache asset

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,18 +1,24 @@
+import CacheHelper from './utils/cache-helper';
+import 'regenerator-runtime';
+
+/* eslint-disable no-restricted-globals */
+const { assets } = global.serviceWorkerOption;
+
 self.addEventListener('install', (event) => {
   console.log('Installing Service Worker ...');
 
-  // TODO: Caching App Shell Resource
+  // TODO: Caching App Shell Container
+  event.waitUntil(CacheHelper.CachingAppContainer([...assets, './']));
 });
 
 self.addEventListener('activate', (event) => {
   console.log('Activating Service Worker ...');
 
   // TODO: Delete old caches
+  event.waitUntil(CacheHelper.deleteOldCache());
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log(event.request);
-
-  event.respondWith(fetch(event.request));
+  event.respondWith(CacheHelper.revalidateCache(event.request));
   // TODO: Add/get fetch request to/from caches
 });
