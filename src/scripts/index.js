0544b42fe3a7b0240b19144ad21c841c2ef21e16
Syahrul-Prayoga <syahrulprayoga@gmail.com>
2021-07-17 02:14:50 +0800

    image optimization (apply lazyload)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,6 +8,8 @@ import '../scripts/components/hero.js';
 import '../scripts/components/footer.js';
 import App from './views/app';
 import swRegister from './utils/sw-register';
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 
 const app = new App({
   button: document.querySelector('#menu'),
